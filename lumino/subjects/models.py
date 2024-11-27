166d85e5c7a9692abba31064e730bee1854e751a
David-Luis-Mora <davidluismora@gmail.com>
2024-11-27 19:22:10 +0000

    modificacion de los modelos

--- lumino/subjects/models.py
+++ lumino/subjects/models.py
@@ -1,21 +1,21 @@
 # Create your models here.
-
+# user.profile.get_role_display
 
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Subject(models.Model):
+    code = models.CharField(unique=True, max_length=255)
     name = models.CharField(max_length=255)
     description = models.TextField(
         blank=True,
     )
-
-    students = models.ManyToManyField(User, related_name='users', blank=True)
+    students = models.ManyToManyField(User, related_name='student_subjects', null=True)
     teacher = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='user_teacher',
+        related_name='teacher_subjects',
     )
 
     def __str__(self):
@@ -23,11 +23,11 @@ class Subject(models.Model):
 
 
 class Lesson(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='lessons')
     title = models.CharField(max_length=255)
     content = models.TextField(
         blank=True,
     )
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='subjects')
 
     def __str__(self):
         return self.title
--- lumino/users/models.py
+++ lumino/users/models.py
@@ -6,9 +6,14 @@ from subjects.models import Subject
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    class Role(models.TextChoices):
+        STUDENT = 'S', 'Student'
+        TEACHER = 'T', 'Teacher'
+
+    role = models.CharField(max_length=1, choices=Role, default=Role.STUDENT)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profiles')
     bio = models.TextField(blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/noavatar.png', blank=True, null=True)
 
     def __str__(self):
         return self.user.username
@@ -18,7 +23,7 @@ class Enrollment(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='enrollments')
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='enrollments')
     enrolled_at = models.DateField(auto_now_add=True)
-    mark = models.IntegerField(null=True, blank=True)
+    mark = models.PositiveSmallIntegerField(null=True)
 
     def __str__(self):
         return f'{self.student.username} - {self.subject.name}'
