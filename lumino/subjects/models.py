d8132b73a091f38207e3d754108c3b0ca7ce3bc5
David-Luis-Mora <davidluismora@gmail.com>
2024-11-27 12:04:59 +0000

    modelo de subject y lesson

--- lumino/subjects/models.py
+++ lumino/subjects/models.py
@@ -1,11 +1,33 @@
 # Create your models here.
 
 
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Subject(models.Model):
-    code = models.CharField(max_length=256)
-    name = models.CharField(max_length=256)
-    teacher = models.ForeignKey('User', related_name='users', on_delete=models.CASCADE)
-    students = models.ForeignKey('User', related_name='users', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(
+        blank=True,
+    )
+
+    students = models.ManyToManyField(User, related_name='users', blank=True)
+    teacher = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_teacher',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(
+        blank=True,
+    )
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='subjects')
+
+    def __str__(self):
+        return self.title
