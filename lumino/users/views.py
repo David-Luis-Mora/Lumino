8d6019006ede51fd9f1f75d390bf840975ffbd12
David-Luis-Mora <davidluismora@gmail.com>
2024-12-02 20:24:15 +0000

    estructura de urls y view de users

--- lumino/users/views.py
+++ lumino/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+# # from django.http import HttpResponseForbidden
+# from django.shortcuts import redirect, render
+
 
 # Create your views here.
+
+
+def user_detail(request):
+    pass
+
+
+def edit_profile(request):
+    pass
+
+
+def request_certificate(request):
+    pass
+
+
+def enroll_subjects(request):
+    pass
+
+
+def unenroll_subjects(request):
+    pass
+
+
+def leave(request):
+    pass
