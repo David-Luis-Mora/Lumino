c32cd06b46facc2b22af4e817b96a5f690f167f3
David-Luis-Mora <davidluismora@gmail.com>
2024-12-02 20:12:17 +0000

    arreglo de urls

--- lumino/subjects/urls.py
+++ lumino/subjects/urls.py
@@ -6,12 +6,12 @@ app_name = 'subjects'
 
 urlpatterns = [
     path('', views.subject_list, name='subject-list'),
-    path('<string:name>/', views.subject_detail, name='subject-detail'),
-    path('<string:name>/lessons/', views.subject_lessons, name='subject_lessons'),
-    path('<string:name>/lessons/<int:pk>/', views.lesson_detail, name='lesson-detail'),
-    path('<string:name>/lessons/add', views.add_lesson, name='lesson-detail'),
-    path('<string:name>/lessons/<int:pk>/edit', views.edit_lesson, name='lesson-detail'),
-    path('<string:name>/lessons/<int:pk>/delete', views.delete_lesson, name='lesson-detail'),
-    path('<string:name>/marks', views.mark_list, name='subject-detail'),
-    path('<string:name>/marks/edit', views.edit_marks, name='subject-detail'),
+    path('<str:name>/', views.subject_detail, name='subject-detail'),
+    path('<str:name>/lessons/', views.subject_lessons, name='subject_lessons'),
+    path('<str:name>/lessons/<int:pk>/', views.lesson_detail, name='lesson-detail'),
+    path('<str:name>/lessons/add', views.add_lesson, name='lesson-detail'),
+    path('<str:name>/lessons/<int:pk>/edit', views.edit_lesson, name='lesson-detail'),
+    path('<str:name>/lessons/<int:pk>/delete', views.delete_lesson, name='lesson-detail'),
+    path('<str:name>/marks', views.mark_list, name='subject-detail'),
+    path('<str:name>/marks/edit', views.edit_marks, name='subject-detail'),
 ]
