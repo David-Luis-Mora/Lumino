28bf0c2072d9e37e9bed2afeffa2a67ad5ed3470
David-Luis-Mora <davidluismora@gmail.com>
2024-12-03 02:42:56 +0000

    cambio de entorno

--- lumino/accounts/views.py
+++ lumino/accounts/views.py
@@ -2,19 +2,19 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from users.models import Profile
 
 from .forms import LoginForm, SignupForm
 
 
 def user_login(request):
-    FALLBACK_REDIRECT = reverse('shared:index')
+    FALLBACK_REDIRECT = reverse('subjects:subject-list')
 
     if request.user.is_authenticated:
         return redirect(FALLBACK_REDIRECT)
     login_error = False
     if request.method == 'POST':
-        next = request.POST.get('next')
+        # next = request.POST.get('next')
+        next = request.POST.get('next', FALLBACK_REDIRECT)
         form = LoginForm(request.POST)
         username = form.data['username']
         password = form.data['password']
@@ -48,10 +48,10 @@ def user_signup(request):
     if request.method == 'POST':
         if (form := SignupForm(request.POST)).is_valid():
             user = form.save()
-            Profile.objects.create(user=user)
+            # Profile.objects.create(user=user)
             login(request, user)
 
-            return redirect('shared:index')
+            return redirect('subjects:subject-list')
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', dict(form=form))
