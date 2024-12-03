488bafb15605767c0a379212f2a78199a47124af
David-Luis-Mora <davidluismora@gmail.com>
2024-12-03 02:34:58 +0000

    modificacion de subject, hecho las primeras 3 vistas

--- lumino/subjects/urls.py
+++ lumino/subjects/urls.py
@@ -6,12 +6,12 @@ app_name = 'subjects'
 
 urlpatterns = [
     path('', views.subject_list, name='subject-list'),
-    path('<str:name>/', views.subject_detail, name='subject-detail'),
-    path('<str:name>/lessons/', views.subject_lessons, name='subject_lessons'),
-    path('<str:name>/lessons/<int:pk>/', views.lesson_detail, name='lesson-detail'),
-    path('<str:name>/lessons/add', views.add_lesson, name='lesson-detail'),
-    path('<str:name>/lessons/<int:pk>/edit', views.edit_lesson, name='lesson-detail'),
-    path('<str:name>/lessons/<int:pk>/delete', views.delete_lesson, name='lesson-detail'),
-    path('<str:name>/marks', views.mark_list, name='subject-detail'),
-    path('<str:name>/marks/edit', views.edit_marks, name='subject-detail'),
+    path('<str:code>/', views.subject_detail, name='subject-detail'),
+    path('<str:code>/lessons/', views.subject_lessons, name='subject_lessons'),
+    path('<str:code>/lessons/<int:pk>/', views.lesson_detail, name='lesson-detail'),
+    path('<str:code>/lessons/add', views.add_lesson, name='lesson-detail'),
+    path('<str:code>/lessons/<int:pk>/edit', views.edit_lesson, name='lesson-detail'),
+    path('<str:code>/lessons/<int:pk>/delete', views.delete_lesson, name='lesson-detail'),
+    path('<str:code>/marks', views.mark_list, name='mark-list'),
+    path('<str:code>/marks/edit', views.edit_marks, name='edit-marks'),
 ]
--- lumino/subjects/views.py
+++ lumino/subjects/views.py
@@ -1,18 +1,47 @@
-# from django.contrib.auth.decorators import login_required
-# # from django.http import HttpResponseForbidden
-# from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+
+# from django.http import HttpResponseForbidden
+from django.shortcuts import render
+from users.models import Profile
+
+from .models import Lesson, Subject
 
 
+@login_required
 def subject_list(request):
-    pass
+    profile = request.user.profiles
+    if profile.role == Profile.Role.TEACHER:
+        subjects = Subject.objects.filter(teacher=request.user)
+    else:  # STUDENT
+        subjects = Subject.objects.filter(students=request.user)
+
+    return render(request, 'subjects/subject_list.html', {'subjects': subjects})
 
 
-def subject_detail(request):
-    pass
+def subject_detail(request, code):
+    subject = Subject.objects.get(code=code)
+    print(request.user)
+    profile = request.user.profiles
+
+    if profile.role == Profile.Role.TEACHER:
+        return render(request, 'subjects/subject_dashboard_teacher.html', {'subject': subject})
+    else:
+        return render(request, 'subjects/subject_dashboard_student.html', {'subject': subject})
 
 
-def subject_lessons(request):
-    pass
+def subject_lessons(request, code):
+    subject = Subject.objects.get(code=code)
+    lessons = Lesson.objects.filter(subject=subject)
+    profile = request.user.profiles
+
+    if profile.role == Profile.Role.TEACHER:
+        return render(
+            request, 'subjects/lessons_teacher.html', {'subject': subject, 'lessons': lessons}
+        )
+    else:
+        return render(
+            request, 'subjects/lessons_student.html', {'subject': subject, 'lessons': lessons}
+        )
 
 
 def lesson_detail(request):
